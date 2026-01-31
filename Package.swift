5af88f76f539b89463ae85db7bbbb2d45a37f5b0
Michael Mueller <michael@mdmueller.me>
2026-01-31 14:55:09 -0600

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCraftShortcuts",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftCraftShortcuts",
             targets: ["SwiftCraftShortcuts"]),
     ],
+    dependencies: [
+        .package(path: "../SwiftCraft")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftCraftShortcuts"),
-
+            name: "SwiftCraftShortcuts",
+            dependencies: ["SwiftCraft"]),
     ]
 )
